c59ecfd8f7af3a6f67aa7cb03a257ce7db8c279a
80883080@qq.com <80883080@qq.com>
2016-12-29 18:17:00 +0800

    版本合并完毕

--- idox/4.5.6说出它的化合价/js/pageController.js
+++ idox/4.5.6说出它的化合价/js/pageController.js
@@ -15,11 +15,8 @@ $(function(){
 		'<h1>Na</h1><h1 class="blue">Cl<p>+1</p></h1>',
 		'<h1>Na</h1><h1 class="blue">N<p>+3</p></h1><h1>O</h1><h2>2</h2>',
 		'<h1 class="blue">Na<p>+1</p></h1><h1>Cl</h1>',
-<<<<<<< HEAD
 		'<h1>Na</h1><h2>2</h2><h1 class="blue">S<p>+6</p></h1><h1>O</h1><h2>4</h2>',
-=======
-		'<h1>Na</h1><h2>2</h2>2<h1 class="blue">S<p>+6</p></h1><h1>O</h1><h2>4</h2>',
->>>>>>> 4d6319a5a0606ea77b5532b4718f113700b89d7e
+
 		'<h1>Na</h1><h2>2</h2><h1 class="blue">S<p>+4</p></h1><h1>O</h1><h2>3</h2>',
 		
 		'<h1>Na</h1><h2>2</h2><h1 class="blue">Si<p>+4</p></h1><h1>O</h1><h2>3</h2>',
@@ -87,16 +84,24 @@ $(function(){
 		]
 		
 		$(".foot-center-box").css({"width":0+"%"});
-		$(".foot-num").html(0+"/15");
+		$(".foot-num").html(1+"/15");
 		$(".hui-btn").removeClass("selectedBtn");
 		var ansArr1=[];
-		qusNum++;
-		$(".foot-num").html(qusNum+"/15");
 		$(".hui-img").addClass("kapai");
 		var mathDom = parseInt(Math.random()*qusArr.length);
 		result=[];
 		socal=0;
-		qusNum=0;
+		qusNum=1;
+		$(".hui-btn").off("click");
+		$(".hui-group").html(qusArr[mathDom]);
+		ansArr1.push(qusArr[mathDom],ansArr[mathDom])
+		qusArr.splice(mathDom,1);
+		ansArr.splice(mathDom,1);
+		$(".hui-btn").on("click",function(){
+			$(this).addClass("selectedBtn");
+			$(".hui-img").removeClass("kapai");
+			pageContorller.selectQus($(this).attr("id"),ansArr1);
+		})
 		
 	})
 	
@@ -126,7 +131,7 @@ $(function(){
 		result.push(arr);
 		socal+=6.666;
 		$(".foot-center-box").css({"width":socal+"%"});
-		if(qusNum==15){
+		if(qusNum==3){
 			pageContorller.socalShow()
 		}else{
 			$(".hui-btn").off("click");
@@ -141,15 +146,10 @@ $(function(){
 			(i-3)%4==0?cla = "class = 'rightBorder'" : cla=""
 			i> 11 ?cla = "class = 'bottomBorder'" : cla=cla
 			if(result[i][1] == result[i][2]){
-<<<<<<< HEAD
 				index+="<li "+cla+"><div class='scoreGroup'>"+result[i][0]+"</div></li>"
 			}else{
 				index+="<li "+cla+"><div class='scoreGroup wrong'>"+result[i][0]+"</div><img src='img/wrong_img.svg'/></li>"
-=======
-				index+="<li "+cla+"><p></p><div class='scoreGroup'>"+result[i][0]+"</div></li>"
-			}else{
-				index+="<li "+cla+"><p></p><div class='scoreGroup wrong'>"+result[i][0]+"</div><img src='img/wrong_img.svg'/></li>"
->>>>>>> 4d6319a5a0606ea77b5532b4718f113700b89d7e
+
 			}
 		}
 		index+="<li></li>"
